fcb98c3146d77cc693d10bd10559fe63baed22f7
Mozes Miko <62065351+MozesMiko@users.noreply.github.com>
2024-11-15 16:35:44 +0100

    feat: add solution to the first 3 exercises

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,14 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+  let output = "";
+  if(num >= 0){
+    for(i = 0; i < num; i++){
+      output += string;
+    }
+  } else {
+    output = "ERROR";
+  }
+  
+  return output;
 };
 
 // Do not edit below this line
