c0bbe8967ed445bde0ba00e9412020b04d296d68
Mozes Miko <62065351+MozesMiko@users.noreply.github.com>
2024-11-18 12:53:38 +0100

    feat: Add solution to exercise 4 - removeFromArray

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,13 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...keyWords) {
+  for(let i = 0; i < keyWords.length; i++){
+    if(array.includes(keyWords[i])){
+      while(array.includes(keyWords[i])){
+        const removeThis = array.indexOf(keyWords[i]);
+        array.splice(removeThis, 1);
+      }
+    }
+  }
+  return array;
 };
 
 // Do not edit below this line
