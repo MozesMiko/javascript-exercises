0fb382d372a69b0caaf2d1fcde13b8f7f6df6fbe
Mozes Miko <62065351+MozesMiko@users.noreply.github.com>
2024-11-18 14:11:32 +0100

    feat: Add solution to exercise 4: leapYears

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,11 @@
-const leapYears = function() {
-
+const leapYears = function(year) {
+  let isItLeap;
+  if((year % 4 == 0 && year % 100 != 0) || year % 400 == 0) {
+    isItLeap = true;
+  } else {
+    isItLeap = false;
+  }
+  return isItLeap;
 };
 
 // Do not edit below this line
