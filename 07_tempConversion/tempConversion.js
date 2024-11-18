9edc7087d5b8bbb18c049134a15e25c1dc6f8ac5
Mozes Miko <62065351+MozesMiko@users.noreply.github.com>
2024-11-18 14:37:41 +0100

    feat: add solution to exercise 7: tempConversion

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(tempFahrenheit) {
+  let celsius = Math.round(((tempFahrenheit -32.0) * (5.0 / 9.0)) * 10) / 10;
+  return celsius; 
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function( tempCelsius) {
+  let fahrenheit = Math.round(((tempCelsius * (9 / 5) + 32)) * 10) / 10;
+  return fahrenheit;
 };
 
 // Do not edit below this line
