692a8b2093b4190442bc7d9e5e4818c589f3a4fa
Mozes Miko <62065351+MozesMiko@users.noreply.github.com>
2024-11-18 14:38:57 +0100

    refactor: made some changes for consistency

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,10 +1,10 @@
 const convertToCelsius = function(tempFahrenheit) {
-  let celsius = Math.round(((tempFahrenheit -32.0) * (5.0 / 9.0)) * 10) / 10;
+  let celsius = Math.round(((tempFahrenheit - 32.0) * (5.0 / 9.0)) * 10) / 10;
   return celsius; 
 };
 
 const convertToFahrenheit = function( tempCelsius) {
-  let fahrenheit = Math.round(((tempCelsius * (9 / 5) + 32)) * 10) / 10;
+  let fahrenheit = Math.round(((tempCelsius * (9.0 / 5.0) + 32.0)) * 10) / 10;
   return fahrenheit;
 };
 
