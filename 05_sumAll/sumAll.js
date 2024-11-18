aac01789080f83bd0b2aad1cdbf4d4779eedeaf9
Mozes Miko <62065351+MozesMiko@users.noreply.github.com>
2024-11-18 13:38:21 +0100

    feat: Add solution to exercise 5: sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,20 @@
-const sumAll = function() {
-
+const sumAll = function(startNumber, endNumber) {
+  let finalSum = 0;
+  if(startNumber < 0 || endNumber < 0 ||
+    !Number.isInteger(startNumber) || !Number.isInteger(endNumber)) {
+    finalSum = "ERROR";
+  } else {
+    if(startNumber < endNumber) {
+      for(let i = startNumber; i <= endNumber; i++){
+        finalSum += i;
+      }
+    } else {
+      for(let i = endNumber; i <= startNumber; i++){
+        finalSum += i;
+      }
+    }
+  }  
+  return finalSum;
 };
 
 // Do not edit below this line
