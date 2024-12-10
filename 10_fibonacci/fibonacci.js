e07f630f9daccd0b0e1a7ee182e66f0c2c6ee668
Mozes Miko <62065351+MozesMiko@users.noreply.github.com>
2024-12-10 15:24:28 +0100

    feat: Add solution to the Fibonacci exercise

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,14 @@
-const fibonacci = function() {
-
+const fibonacci = function(num) {
+  let int = Number(num);
+  if(int >= 0) {
+    let fib = [0, 1];
+    for(let i = 2; i <= int; i++) {
+    fib[i] = fib[i - 2] + fib[i - 1];
+    }
+    return fib[int];
+  } else {
+    return "OOPS";
+  }  
 };
 
 // Do not edit below this line
