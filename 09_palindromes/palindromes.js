bd2c1812f5308cbb25667cb2a5097389f8f0a875
Mozes Miko <62065351+MozesMiko@users.noreply.github.com>
2024-12-10 15:09:56 +0100

    feat: Add solution to the Palindromes exercise

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,14 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+  let lowerCase = str.toLowerCase();
+  let noPunc = lowerCase.replace(/[^a-zA-Z0-9]/g, '');
+  let arr = noPunc.split("");
+  arr.reverse();
+  let output = arr.join("");
+  if(noPunc === output) {
+    return true;
+  } else {
+    return false;
+  }
 };
 
 // Do not edit below this line
